9b3a1413edfc5ced84edec365703c6014bc8e8ac
Khaair <khair.cse.iu@gmail.com>
2021-12-29 13:14:03 +0600

    update

--- import01.js
+++ import01.js
@@ -1,6 +1,13 @@
+import * as stringFunctions from './string_functions.js';
 import { lowercaseString, uppercaseString } from './string_functions.js';
 
 // Only change code above this line
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
