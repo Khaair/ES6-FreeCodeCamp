d7b9ea9b4d4928592c0749dfdea8d334cf8afe27
Khaair <khair.cse.iu@gmail.com>
2021-12-29 12:43:42 +0600

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <!-- Only change code below this line -->
+
+    <!-- Only change code above this line -->
+
+    <script type="module" src="index.js"></script>
+  </body>
+</html>
